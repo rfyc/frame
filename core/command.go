87c272cf97325d829408e285bb1b062fd9dc1714
洪伟 <46773675+rfyc@users.noreply.github.com>
2022-03-10 20:31:41 +0800

    cmd

--- core/command.go
+++ core/command.go
@@ -26,7 +26,52 @@ type command struct {
 	desc    map[string]string
 }
 
+func (this *command) init() {
+
+	if this.actions == nil {
+		this.actions = map[string]iAction{}
+	}
+	if this.args == nil {
+		this.args = map[string]iArgs{}
+	}
+	if this.input == nil {
+		this.input = map[string]string{}
+	}
+	if this.cmds == nil {
+		this.cmds = map[string]func(){}
+	}
+	if this.desc == nil {
+		this.desc = map[string]string{}
+	}
+}
+
+func (this *command) RegisterArgs(name string, args iArgs, desc ...string) {
+	this.init()
+	this.args[name] = args
+	if len(desc) > 0 {
+		this.desc["args_"+name] = desc[0]
+	}
+}
+
+func (this *command) RegisterAction(name string, action iAction, desc ...string) {
+	this.init()
+	this.actions[name] = action
+	if len(desc) > 0 {
+		this.desc["action_"+name] = desc[0]
+	}
+}
+
+func (this *command) RegisterCmd(name string, runCmd func(), desc ...string) {
+	this.init()
+	this.cmds[name] = runCmd
+	if len(desc) > 0 {
+		this.desc["cmd_"+name] = desc[0]
+	}
+}
+
 func (this *command) Run(app iApp) {
+
+	this.init()
 
 	for _, arg := range this.initArgs() {
 		if input, err := json.Marshal(this.parseInput()); err != nil {
@@ -70,33 +115,4 @@ func (this *command) parseAction() (name string, action iAction) {
 		}
 	}
 	return
-}
-
-func (this *command) RegisterArgs(name string, args iArgs, desc ...string) {
-
-	this.initArgs()
-	this.args[name] = args
-	if len(desc) > 0 {
-		this.desc["args_"+name] = desc[0]
-	}
-}
-
-func (this *command) RegisterAction(name string, action iAction, desc ...string) {
-	if this.actions == nil {
-		this.actions = map[string]iAction{}
-	}
-	this.actions[name] = action
-	if len(desc) > 0 {
-		this.desc["action_"+name] = desc[0]
-	}
-}
-
-func (this *command) RegisterCmd(name string, runCmd func(), desc ...string) {
-	if this.cmds == nil {
-		this.cmds = map[string]func(){}
-	}
-	this.cmds[name] = runCmd
-	if len(desc) > 0 {
-		this.desc["cmd_"+name] = desc[0]
-	}
 }
